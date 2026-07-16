310923a52b3360913b0b56b91caae2ea2496a029
Ilhmm27 <arramadhanilham@gmail.com>
2026-07-16 18:38:01 +0700

    Upload portfolio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const text = "Ilham AS";
+let i = 0;
+
+function typing() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 150);
+    }
+}
+
+typing();
